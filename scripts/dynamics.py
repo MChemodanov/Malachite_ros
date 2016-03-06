1f72d53b683dacd6827d753b3b998e99d316284b
Mikhail Chemodanov <mikhail.chemodanov@gmail.com>
2016-03-06 20:52:40 +0300

    Lesson 4. Dynamics, PID

---
+++ scripts/dynamics.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import rospy
+import math
+from geometry_msgs.msg import Twist
+
+rospy.init_node("dynamics")
+
+inertia_moment = rospy.get_param("~moment_inertia")
+mass = rospy.get_param("~mass")
+gamma = rospy.get_param("~gamma")
+
+frequency = 100
+
+state = Twist()
+effort = Twist()
+
+def callback(msg):
+  global effort
+  effort = msg
+
+p = rospy.Publisher("turtle1/cmd_vel", Twist)
+s = rospy.Subscriber("turtle1/dynamics", Twist, callback)
+
+rate = rospy.Rate(frequency)
+
+def calculate_step():
+  linear_a = effort.linear.x  - state.linear.x**2*gamma
+  linear_a /= mass
+  state.linear.x += linear_a/frequency
+
+  angular_a = effort.angular.z - state.angular.z**2*gamma
+  angular_a /= inertia_moment
+  state.angular.z += angular_a/frequency
+  
+  p.publish(state)
+
+while not rospy.is_shutdown():
+  calculate_step()
+  rate.sleep()
